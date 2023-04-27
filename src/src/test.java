7285bcae21558b4b416b638a44f26c89910d2e98
studheigpa2677 <patrick.heigl@stud.th-rosenheim.de>
2023-04-27 19:48:03 +0200

    Deleted unnecessary files and worde a test line of code in test-class

---
+++ src/src/test.java
@@ -0,0 +1,5 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("This is a test output for a test class in a test projekt for testing purpose only");
+    }
+}
